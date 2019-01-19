70cbea113bc6cf9de0a69074b1344ccdf86770aa
Aleksandar Drazhev <aleksandar.drazhev@paysafe.com>
2019-01-20 01:47:53 +0200

    Initial Commit

---
+++ DonutViewTechnicalAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DonutViewTechnicalAssignment
+//
+//  Created by Aleksandar Drazhev on 20.01.19.
+//  Copyright © 2019 Aleksandar Drazhev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
